1518f297adc057da00a3727bdeae5d0a46f7001e
Narayanaswamy <chinthanarayanaswamy2641@gmail.com>
2025-03-07 10:21:27 +0530

    new

--- src/A.java
+++ src/A.java
@@ -4,6 +4,7 @@ public class A {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
        System.out.println("Welcome to the New World");
+       System.out.println("All ways comes");
 	}
 
 }
